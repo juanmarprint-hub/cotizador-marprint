ab11fd17fd92a17c85e5b60251a2fa25622d511e
juanmarprint-hub <juan.marprint@gmail.com>
2026-05-19 01:33:36 -0500

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Cotizador Marprint</h1>
-      <p>Primer módulo funcionando 🚀</p>
-      <button>Crear Cliente</button>
-    </main>
-  );
-}
