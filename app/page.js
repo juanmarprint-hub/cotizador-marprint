75ee009147e1fd95a59f1ab7d5e045c58f838278
juanmarprint-hub <juan.marprint@gmail.com>
2026-05-19 01:28:38 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>Cotizador Marprint</h1>
       <p>Primer módulo funcionando 🚀</p>
       <button>Crear Cliente</button>
-    </div>
+    </main>
   );
 }
