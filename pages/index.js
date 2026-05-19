8a1a925d28f69e358dd6a758614d379e90025bf6
juanmarprint-hub <juan.marprint@gmail.com>
2026-05-19 01:32:55 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Cotizador Marprint</h1>
+      <p>Primer módulo funcionando 🚀</p>
+
+      <button
+        style={{
+          padding: "12px 20px",
+          background: "#111",
+          color: "white",
+          border: "none",
+          borderRadius: "8px",
+          cursor: "pointer",
+        }}
+      >
+        Crear Cliente
+      </button>
+    </div>
+  );
+}
